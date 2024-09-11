f63c5e6860b92f0ca2aef0ece1cdf8a643bdb983
Valentin <zhe-valentin@mail.ru>
2024-09-11 23:39:57 +0700

    Course work "Introduction to the profession and syntax of the language" Базовая сложность

---
+++ src/Employee.java
@@ -0,0 +1,69 @@
+import java.util.Objects;
+
+public class Employee {
+    private static int idCounter = 1; // Статическая переменная-счетчик
+    private final int ID;
+    private final String FULL_NAME;  // Поле ФИО
+    private int department;   // Поле отдел
+    private double salary;    // Поле зарплата
+
+
+    // Конструктор
+    public Employee(String FULL_NAME, int department, double salary) {
+        ID = idCounter++; //Присвоение ID из счетчика и увеличение счетчика
+        this.FULL_NAME = FULL_NAME;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Геттеры
+    public String getFullName() {
+        return this.FULL_NAME;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    // Сеттеры
+    public void setDepartment(int department) {
+        if (department > 0 && department <= 5) {
+            this.department = department;
+        } else {
+            throw new IllegalArgumentException("Отдел должен быть от 1 до 5.");
+        }
+    }
+
+    public void setSalary(double salary) {
+        if (salary >= 0) {
+            this.salary = salary;
+        } else {
+            throw new IllegalArgumentException("Зарплата не может быть отрицательной.");
+        }
+    }
+
+    // Переопределение equals и hashCode
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Employee employee = (Employee) obj;
+        return department == employee.department && Double.compare(salary, employee.salary) == 0 && ID == employee.ID && Objects.equals(FULL_NAME, employee.FULL_NAME);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(FULL_NAME, department, salary, ID);
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Сотрудник {" + "id=" + ID + " Ф.И.О. '" + FULL_NAME + '\'' +
+                ", отдел = " + department + ", зарплата = " + salary + '}';
+    }
+}
